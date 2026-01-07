d0798e55eb270180b89ca1f609826fd3ca340d3f
Monisss <monishedu@gmail.com>
2026-01-07 21:44:33 +0530

    begginers python revision

--- learning-1.py
+++ learning-1.py
@@ -1,1 +1,24 @@
-print ("hello boi")
+# learning strings
+print("Hello World! \n Hello Moniss \n Hello Cybes \n Hello Bully")
+
+print("Hey" + "Hoeelo" + "Ooo  my Goood")
+
+ikr = input("What is ur fav anime?")
+print(ikr)
+
+okay = "Bully" 
+print(okay)
+
+ikr = "Boruto" 
+print(ikr)
+
+print(ikr)
+
+# Beginner project 1
+
+print ("Welcome to band name generator")
+city = input("Enter the city that you grew up in..")
+print(city)
+pet =input("Enter the name of your fav pet!")
+print(pet)
+print("Your band name could be" + city + pet)
