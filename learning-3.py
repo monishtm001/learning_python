9ba7f49f1dd885974fff9414919557efad2d6e95
Monisss <monishedu@gmail.com>
2026-01-09 20:10:34 +0530

    Pizza Bill

--- learning-3.py
+++ learning-3.py
@@ -15,7 +15,7 @@ height=int(input("please enter your height in cm:"))
 if height>=120:
     print("you are eligible for a ride.")
     age=int(input("enter your age:"))
-    elif age>=18:
+    if age>=18:
         print("your bill will be $20")
     elif age<=12:
         print("Your bill will be $5")
@@ -23,3 +23,24 @@ if height>=120:
         print("your bill will be $7")
 else:
     print("Your not eligible for a ride")
+
+# Creating PIZZA delivery final bill
+
+print("Welcome to Moniss Pizza Delivery")
+size = str(input("Enter which size pizza u want? S/M/L:"))
+pproni = input("Do u want pproni in your pizza? Y for Yes and N for No")
+xtra_cheese = input("Do u want extra cheese in your pizza? Y / N")
+bill = 0
+if size == "S":
+    bill=5
+elif size == "M":
+    bill = 10
+elif size == "L":
+    bill = 20
+if pproni == "Y":
+    bill=bill+5
+if xtra_cheese == "Y":
+    bill=bill+7
+    print("Your Bill is:" + str(bill))
+else:
+    print("Print Valid Value")
