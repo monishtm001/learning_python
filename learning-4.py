3d89c85c8bf0843b9dbd99773e3956ebc9f9ae9d
Monisss <monishedu@gmail.com>
2026-01-10 01:28:12 +0530

    Loops

---
+++ learning-4.py
@@ -0,0 +1,20 @@
+import random
+one_zero = random.randint(2,5)
+print(one_zero)
+rezero = random.random()*9
+print(rezero)
+ikrep = random.uniform(5,125)
+print(ikrep)
+
+
+friends=["Moniss","Bully","Vade","putta","Bobby"]
+choose = random.choice(friends)
+print(choose)
+
+
+# Loops
+scores = [78, 85, 92, 88, 76, 95, 89]
+high=max(scores)
+
+for score in scores:
+    print(high)
