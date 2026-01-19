f7e59a790c3b54b3e8a46c68a7d4c2435fed97f0
Monisss <monishedu@gmail.com>
2026-01-19 14:39:48 +0530

    Learning Dictionaries

---
+++ learning-7.py
@@ -0,0 +1,16 @@
+# Dictionaries 
+anime_name={"Nnum_1":"One Piece is the number one aniem in the world.",
+            "Num_2":"Naruto is the number two anime in the world.",
+            "Num_3456":["Bleach","MHA","Black Clover","Demon Slayer"]
+}
+
+print(anime_name["Num_2"])
+
+print(anime_name["Num_3456"][2])
+
+anime_name["Num_3456"]=["ReZero","Haikyuu","Promised Never Land"]
+
+print(anime_name["Num_3456"])
+
+anime_name={}
+print(anime_name) 
