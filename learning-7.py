4cfef4657e61d007d78f8a3fcf28c53a2892fc1f
Monisss <monishedu@gmail.com>
2026-01-20 21:34:11 +0530

    Dictionaries

--- learning-7.py
+++ learning-7.py
@@ -12,5 +12,17 @@ anime_name["Num_3456"]=["ReZero","Haikyuu","Promised Never Land"]
 
 print(anime_name["Num_3456"])
 
-anime_name={}
-print(anime_name) 
+
+
+anime_watched={
+    "One_Piece":{
+    "Charecters":["Zoro","Luffy","Name","Robin"],
+    "num_times_watched":1,
+    },
+    "Haikyuu":{
+    "Charecters":["Hinata","Kageyama","Oikawa","Miya Twins"],
+    "Num_times_wateched":100
+    }
+}
+
+print(anime_watched["Haikyuu"]["Charecters"][2])
