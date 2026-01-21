f2422c2f642125f10294f40e43b4de74c53e44d9
Monisss <monishedu@gmail.com>
2026-01-21 12:26:46 +0530

    Functions with op

---
+++ learning-8.py
@@ -0,0 +1,5 @@
+def full_name():
+    f_name=input("Enter your First Name:").title()
+    l_name=input("Enter Your Last name:").title()
+    return f"your full name is {f_name} {l_name}"
+print(full_name())
