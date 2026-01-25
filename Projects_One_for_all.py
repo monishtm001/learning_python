96536f2ab458b3aec00420fba42185f269050d20
Monisss <monishedu@gmail.com>
2026-01-25 17:52:30 +0530

    Final Chanmges

--- Projects_One_for_all.py
+++ Projects_One_for_all.py
@@ -1,84 +1,84 @@
 # Password Generator
 import random
-# letters = [
-#     "a","b","c","d","e","f","g","h","i","j",
-#     "k","l","m","n","o","p","q","r","s","t",
-#     "u","v","w","x","y","z"
-# ]
-# numbers = ["0","1","2","3","4","5","6","7","8","9"]
-# symbols = [
-#     "!", "@", "#", "$", "%", "^", "&", "*",
-#     "(", ")", "_", "+", "-", "=", "{", "}",
-#     "[", "]", ":", ";", "<", ">", "?", "/"
-# ]
+letters = [
+    "a","b","c","d","e","f","g","h","i","j",
+    "k","l","m","n","o","p","q","r","s","t",
+    "u","v","w","x","y","z"
+]
+numbers = ["0","1","2","3","4","5","6","7","8","9"]
+symbols = [
+    "!", "@", "#", "$", "%", "^", "&", "*",
+    "(", ")", "_", "+", "-", "=", "{", "}",
+    "[", "]", ":", ";", "<", ">", "?", "/"
+]
 
-# num_numbers=int(input("Enter number of numbers u want to include in your password:"))
-# num_letters=int(input("Enter number of letters u want to include in your password:"))
-# num_symbols=int(input("Enter number of symbols u want to include in your password:"))
+num_numbers=int(input("Enter number of numbers u want to include in your password:"))
+num_letters=int(input("Enter number of letters u want to include in your password:"))
+num_symbols=int(input("Enter number of symbols u want to include in your password:"))
 
-# pass_in_list=[]
+pass_in_list=[]
 
-# for i in range(num_letters):
-#     pass_in_list.append(random.choice(letters))
+for i in range(num_letters):
+    pass_in_list.append(random.choice(letters))
 
-# for n  in range(num_numbers):
-#     pass_in_list.append(random.choice(numbers))
+for n  in range(num_numbers):
+    pass_in_list.append(random.choice(numbers))
 
-# for s  in range(num_symbols):
-#     pass_in_list.append(random.choice(symbols))
+for s  in range(num_symbols):
+    pass_in_list.append(random.choice(symbols))
 
-# random.shuffle(pass_in_list)
+random.shuffle(pass_in_list)
 
-# char=""
-# for word in pass_in_list:
-#     char+=word
-# print(char)
+char=""
+for word in pass_in_list:
+    char+=word
+print(char)
 
 ## Hang Man Game
 
-# words = [
-#     "python", "coding", "developer", "programming", "computer",
-#     "keyboard", "internet", "software", "hardware", "network",
-#     "hangman", "password", "security", "encryption", "hacker",
-#     "treasure", "island", "pirate", "adventure", "mystery",
-#     "machine", "learning", "artificial", "intelligence", "robot",
-#     "science", "engineer", "technology", "future", "innovation",
-#     "school", "college", "student", "teacher", "education",
-#     "football", "cricket", "basketball", "tennis", "chess",
-#     "music", "guitar", "piano", "drums", "concert",
-#     "mountain", "river", "forest", "desert", "ocean",
-#     "travel", "journey", "explore", "discovery", "freedom",
-#     "friendship", "family", "happiness", "success", "dream",
-#     "camera", "mobile", "laptop", "tablet", "device",
-#     "history", "culture", "language", "literature", "art"
-# ]
+words = [
+    "python", "coding", "developer", "programming", "computer",
+    "keyboard", "internet", "software", "hardware", "network",
+    "hangman", "password", "security", "encryption", "hacker",
+    "treasure", "island", "pirate", "adventure", "mystery",
+    "machine", "learning", "artificial", "intelligence", "robot",
+    "science", "engineer", "technology", "future", "innovation",
+    "school", "college", "student", "teacher", "education",
+    "football", "cricket", "basketball", "tennis", "chess",
+    "music", "guitar", "piano", "drums", "concert",
+    "mountain", "river", "forest", "desert", "ocean",
+    "travel", "journey", "explore", "discovery", "freedom",
+    "friendship", "family", "happiness", "success", "dream",
+    "camera", "mobile", "laptop", "tablet", "device",
+    "history", "culture", "language", "literature", "art"
+]
 
-# ran_word=random.choice(words)
-# len_word=len(ran_word)
-# print(f"Your pick of the day word has {ran_word} letters")
-# Display_let_in_list=["_"]*len_word
-# str_of_list=" ".join(Display_let_in_list)
-# print(str_of_list)
-# LIFES=6
+ran_word=random.choice(words)
+len_word=len(ran_word)
+print(f"Your pick of the day word has {ran_word} letters")
+Display_let_in_list=["_"]*len_word
+str_of_list=" ".join(Display_let_in_list)
+print(str_of_list)
+LIFES=6
 
 
-# while "_" in Display_let_in_list and LIFES> 0:
-#     ask_let=input("Enter a Random Letter").lower()
-#     found=False
-#     for i in range(len_word):
-#         if ran_word[i]==ask_let:
-#             Display_let_in_list[i]=ask_let
-#             found=True
-#     if not found:
-#         LIFES-=1
-#         print(f"Wrong Guess: {LIFES} left ")
-#     if LIFES == 0:
-#         print(f"GAME OVER, The Word was = {ran_word} ")
-#         break
-#     str_of_list=" ".join(Display_let_in_list)
-#     print(str_of_list)
-# if "_" not in Display_let_in_list:
-#     print("Congragulations You Won")
+while "_" in Display_let_in_list and LIFES> 0:
+    ask_let=input("Enter a Random Letter").lower()
+    found=False
+    for i in range(len_word):
+        if ran_word[i]==ask_let:
+            Display_let_in_list[i]=ask_let
+            found=True
+    if not found:
+        LIFES-=1
+        print(f"Wrong Guess: {LIFES} left ")
+    if LIFES == 0:
+        print(f"GAME OVER, The Word was = {ran_word} ")
+        break
+    str_of_list=" ".join(Display_let_in_list)
+    print(str_of_list)
+if "_" not in Display_let_in_list:
+    print("Congragulations You Won")
     
             
 ## Calculator
